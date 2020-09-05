d2e034bf9d3dbad03785e3c67d08d90ff14d508a
briansamu <brian@xonndevelopment.com>
2020-09-05 04:15:29 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Form Blur Event Listners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+  const name = document.getElementById('name');
+  // const re = //i;
+
+}
+
+function validateZip() {
+
+}
+
+function validateEmail() {
+
+}
+
+function validatePhone() {
+
+}
+
